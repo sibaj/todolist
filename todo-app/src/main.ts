f464708124740a4b0d9b6bb515c967c4e848abde
sibaj <sibajyoti.it.imps@gmail.com>
2025-11-26 20:34:00 +0530

    msalInstance initialize

--- todo-app/src/main.ts
+++ todo-app/src/main.ts
@@ -1,5 +1,13 @@
+import { PublicClientApplication } from '@azure/msal-browser';
 import { AppModule } from './app/app.module';
 import { platformBrowser } from '@angular/platform-browser';
+import { msalConfig } from './app/auth-config';
+
+
+export const msalInstance = new PublicClientApplication(msalConfig);
+
+await msalInstance.initialize();
+
 
 platformBrowser()
 	.bootstrapModule(AppModule)
