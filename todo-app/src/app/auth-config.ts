88c07a6873e774d05d6419dcbf699320b8c0722b
sibaj <sibajyoti.it.imps@gmail.com>
2025-11-26 20:10:53 +0530

    redirect url updated

--- todo-app/src/app/auth-config.ts
+++ todo-app/src/app/auth-config.ts
@@ -6,7 +6,7 @@ export const msalConfig: Configuration = {
   auth: {
     clientId: '64d6a144-d25d-45b2-ae5d-f22b6cd45f91', // replace with your Azure AD app client id
     authority: 'https://login.microsoftonline.com/801949e9-b44a-44cc-b66c-4081418c9d5b', // replace with your tenant id or common
-    redirectUri: 'http://localhost:4200'
+    redirectUri: 'https://victorious-sea-001e1471e.3.azurestaticapps.net'//'http://localhost:4200'
   },
   cache: {
     cacheLocation: 'localStorage',
